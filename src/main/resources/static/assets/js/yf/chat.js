27cddbf2a9dfdd62af1ce50ad4cd6d16e127b018
aLowMagic <thePrestige_yf@outlook.com>
2020-04-07 17:18:57 +0800

    ~

---
+++ src/main/resources/static/assets/js/yf/chat.js
@@ -0,0 +1,80 @@
+window.onload = scrollToBottom();
+function keyDownSearch(e){
+    var event = e || window.event;
+    var code = event.keyCode || event.which || event.charCode;
+    if(code==13) {
+        var text = document.getElementById('msg').value;
+        if(text != "\n" && text!=null&&text!=""){
+            SendMsg();
+            window.event.returnValue = false;
+            e.preventDefault();
+        }else{
+            window.event.returnValue = false;
+            e.preventDefault();
+            text.value = "";
+        }
+    }
+}
+
+function scrollToBottom(){
+    var scroll = document.getElementById("dialogWindow");
+    scroll.scrollTop = scroll.scrollHeight;
+    document.getElementById('msg').focus();
+}
+
+function SendMsg()
+{
+    var text = document.getElementById("msg");
+    if (text.value != "" && text.value != null && text.value!="\n")
+    {
+        AddMsg('user', SendMsgDispose(text.value));
+        text.value = "";
+    }
+    scrollToBottom();
+}
+
+
+// 发送的信息处理
+function SendMsgDispose(detail)
+{
+    detail = detail.replace(" ", "&nbsp;");
+    return detail;
+}
+
+// 增加信息
+function AddMsg(user,content)
+{
+    newDiv = CreadMsg(user, content);
+    var msgs = document.getElementById("msgCard");
+    msgs.appendChild(newDiv);
+}
+
+// 生成内容
+function CreadMsg(user, content)
+{
+    newDiv = document.createElement("div");
+    if(user == 'user')
+    {
+        newDiv.setAttribute("class", "msgright");
+        // str = "<div class=\"msgright\">"+
+        //     "<img class=\"chat_head\" src=\"../static/assets/img/dsp.png\">" +
+        //     "<p class=\"msgText\">"+content+"</p>" +"</div>";
+    }
+    else
+    {
+        newDiv.setAttribute("class", "msgleft");
+        // str="<div class=\"msgleft\">" +
+        //     "<img class=\"chat_head\" src=\"../static/assets/img/dsp.png\">\n" +
+        //     "<p class=\"msgText\">"+content+"</p>\n" +
+        //     "</div>";
+    }
+    newImg = document.createElement("img");
+    newImg.setAttribute("class", "chat_head");
+    newImg.setAttribute("src", "./static/assets/img/dsp.png");
+    newP = document.createElement("p");
+    newP.setAttribute("class", "msgText");
+    newP.innerText = content;
+    newDiv.append(newImg);
+    newDiv.append(newP);
+    return newDiv;
+}
